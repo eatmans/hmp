a72c04a6ce99c8a6540a0be41353a44fcc7e4007
eatmans <eatmans@163.com>
2021-09-18 17:24:31 +0800

    修改readme

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+go build -o cmd/hmpc.exe client/hmpc.go
+
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build hmps.go
+go build -o cmd/hmps server/hmps.go
--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build hmps.go
+go build -o cmd/hmps server/hmps.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o cmd/hmpc.exe client/hmpc.go
